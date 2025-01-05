1a81d43df8db0a6a50904c8de1a32a707cb06c4c
iamPavan17 <iampavan05@gmail.com>
2025-01-05 19:28:41 +0530

    fetching data with server components

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderSnippets = snippets.map((snippet) => (
+    <div key={snippet.id}>{snippet.title}</div>
+  ));
+
+  return <div>{renderSnippets}</div>;
 }
