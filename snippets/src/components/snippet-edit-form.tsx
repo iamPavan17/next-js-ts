9a46f6d17c8dd88bfdea523e0e3a92c772312334
iamPavan17 <iampavan05@gmail.com>
2025-03-17 21:24:13 +0530

    calling a server action from a client component

--- snippets/src/components/snippet-edit-form.tsx
+++ snippets/src/components/snippet-edit-form.tsx
@@ -17,6 +17,8 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
     setCode(value);
   };
 
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
+
   return (
     <div>
       <Editor
@@ -30,6 +32,12 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         }}
         onChange={handleEditorChange}
       />
+
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
